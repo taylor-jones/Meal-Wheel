0ae72c35bc6e7bd29d3ce6ab321389313210089e
asoback <31739297+asoback@users.noreply.github.com>
2018-07-16 23:55:35 -0500

    Working on index page.
    Figuring out how to make wheel spin. Click wheel.

---
+++ app/public/app.js
@@ -0,0 +1,27 @@
+document.getElementById("wheelImg").addEventListener('click', function(){
+	rotateAnimation("wheelImg", 10, 0)
+});
+
+//help from http://www.developphp.com/video/JavaScript/Transform-Rotate-Image-Spin-Smooth-Animation-Tutorial
+var looper;
+var degrees = 0;
+function rotateAnimation(el,speed, count){
+	var elem = document.getElementById("wheelImg");
+	if(navigator.userAgent.match("Chrome")){
+		elem.style.WebkitTransform = "rotate("+degrees+"deg)";
+	} else if(navigator.userAgent.match("Firefox")){
+		elem.style.MozTransform = "rotate("+degrees+"deg)";
+	} else if(navigator.userAgent.match("MSIE")){
+		elem.style.msTransform = "rotate("+degrees+"deg)";
+	} else if(navigator.userAgent.match("Opera")){
+		elem.style.OTransform = "rotate("+degrees+"deg)";
+	} else {
+		elem.style.transform = "rotate("+degrees+"deg)";
+	}
+	count++;
+	if(degrees < 361){
+		looper = setTimeout('rotateAnimation(\''+el+'\','+speed+', ' + count +')',speed);
+	}
+	degrees++;
+	
+}
