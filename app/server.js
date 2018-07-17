6ac0c882379c4e084053e2fdff2a9f3ef7f2db0f
Andrew <sobacka@gmail.com>
2018-07-17 09:12:12 -0700

    Server now works

--- app/server.js
+++ app/server.js
@@ -8,21 +8,27 @@ var express = require("express");
 var app = express();
 
 app.set('port', 31001);
-//app.use("public", express.static('public'));
-<<<<<<< HEAD
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
-=======
+app.use(express.static('public'));
+
 //app.set('view engine', 'ejs');
 //app.engine('html', require('ejs').renderFile);
->>>>>>> d1de5513bf0d16ffdac74c38ede9e1fe009d9c07
+
+//app.set('view engine', 'ejs');
+//app.engine('html', require('ejs').renderFile);
 
 app.get('/', function(req, res){
   res.redirect('/index.html');
 });
 
+/*
 app.post('/', function(req, res){
  res.redirect('index.html');
+});
+*/
+
+app.use(function(req, res){
+	res.status(404);
+	res.redirect('/404.html');
 });
 
 app.listen(app.get('port'), function(){
