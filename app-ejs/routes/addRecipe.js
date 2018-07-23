9abbbaf1dce58ce860665b5d0258e5d59d9d7446
asoback <31739297+asoback@users.noreply.github.com>
2018-07-23 16:09:44 -0500

    Updating new recipe page

--- app-ejs/routes/addRecipe.js
+++ app-ejs/routes/addRecipe.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+const cuisines = require('../data/cuisines.json');
+const categories = require('../data/recipe-categories.json');
+const units = require('../data/units-of-measure.json');
+
 /* GET add recipe page. */
 router.get('/', (req, res, next) => {
   res.render('addRecipe', {
     page: 'Add Recipe',
     menuId: 'addRecipe',
+    cuisines: cuisines,
+    categories: categories,
+    units: units
   });
 });
 
