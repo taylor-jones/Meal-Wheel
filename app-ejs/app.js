3f36df931bf5a6c0d529a32ea4c33b08226b0781
Taylor Jones <taylorjonesdev@gmail.com>
2018-07-23 17:14:51 -0500

    Remved unnecessary line from app.js

--- app-ejs/app.js
+++ app-ejs/app.js
@@ -29,7 +29,6 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
