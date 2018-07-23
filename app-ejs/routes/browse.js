081ecd9e5c79dcee01d576015250a6c926adaf7d
Taylor Jones <taylorjonesdev@gmail.com>
2018-07-23 13:19:46 -0500

    Started working on recipe browsing

--- app-ejs/routes/browse.js
+++ app-ejs/routes/browse.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const cuisines = require('../data/cuisines.json');
 const categories = require('../data/recipe-categories.json');
 const diets = require('../data/dietary-restrictions.json');
+const recipes = require('../data/recipes.json');
 
 /* GET browse page. */
 router.get('/', (req, res, next) => {
@@ -13,6 +14,7 @@ router.get('/', (req, res, next) => {
     cuisines: cuisines,
     categories: categories,
     diets: diets,
+    recipes: recipes,
   });
 });
 
