09cf81c8ce039ce485f0dd92b0f71452df7b6e16
asoback <31739297+asoback@users.noreply.github.com>
2018-07-24 10:25:59 -0500

    forgot to delete duplicate variables

--- app-ejs/routes/addRecipe.js
+++ app-ejs/routes/addRecipe.js
@@ -3,9 +3,6 @@ var mysql = require('../bin/dbcon.js');
 const express = require('express');
 const router = express.Router();
 
-const cuisines = require('../data/cuisines.json');
-const categories = require('../data/recipe-categories.json');
-const units = require('../data/units-of-measure.json');
 
 /* GET add recipe page. */
 router.get('/', (req, res, next) => {
