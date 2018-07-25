1ba69de4a128c5845bbbd8b2a5a54f650da8fde3
asoback <31739297+asoback@users.noreply.github.com>
2018-07-24 20:46:06 -0500

    merging app-ejs and app folders

--- app/routes/addRecipe.js
+++ app/routes/addRecipe.js
@@ -1,19 +1,28 @@
+var mysql = require('../bin/dbcon.js');
+
 const express = require('express');
 const router = express.Router();
 
-const cuisines = require('../data/cuisines.json');
-const categories = require('../data/recipe-categories.json');
-const units = require('../data/units-of-measure.json');
 
 /* GET add recipe page. */
 router.get('/', (req, res, next) => {
-  res.render('addRecipe', {
-    page: 'Submit a new Recipe',
-    menuId: 'addRecipe',
-    cuisines: cuisines,
-    categories: categories,
-    units: units,
-  });
+
+	mysql.pool.query('SELECT cuisine_id, cuisine_name FROM cuisine', function(err, rows, fields){
+		const cuisines = rows;
+		mysql.pool.query('SELECT recipe_category_id, recipe_category_name FROM recipe_category', function(err, rows, fields){
+			const categories = rows;
+			mysql.pool.query('SELECT unit_of_measure_id, unit_of_measure_name FROM unit_of_measure', function(err, rows, fields){
+				const units = rows;
+				res.render('addRecipe', {
+				    page: 'Sumbit a new Recipe',
+				    menuId: 'addRecipe',
+				    cuisines: cuisines,
+				    categories: categories,
+				    units: units
+			  	});
+			});
+		});
+	});
 });
 
 module.exports = router;
