0683cdc2b24e5068cde4c77d34d0ec88acc36c50
Taylor Jones <taylorjonesdev@gmail.com>
2018-07-26 22:59:25 -0500

    Started working on random recipe display (see comments) I started working on the POST request from the home page form that will retrieve a random recipe. Right now it's just calling a POST on '/', which requires a page refresh (which I don't think is the way we want to go), but I'm justtrying to get a feel for how we can do all this still. Seems we should convert it to an AJAX POST request where we c an update the recipe w/o a refresh? Also, I added the EJS to check for a recipe on the homepage, and show it if it exists (I think this may change too though, once we go AJAX route). I removed the 'recipeCard' from the Partials, because it was way too big for the homepage, so I changed the look of a home recipe to be more akin to the homepage form. Of course, we can talk more about  how we really want it to look.

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const Categories = require('../controllers/RecipeCategory');
 const Cuisines = require('../controllers/Cuisine');
 const Diets = require('../controllers/DietaryRestriction');
+const Recipes = require('../controllers/Recipe');
 
 
 /* GET home page. */
@@ -22,5 +23,48 @@ router.get('/', (req, res, next) => {
     });
   });
 });
+
+
+
+/* GET home page after wheel spin (with recipe) */
+
+/**
+ * There's a lot that's not complete with this:
+ * 1) There's no DB query to determine the recipe right now. It's
+ *    just generating a random number between 1-3, since I know we have
+ *    3 recipes in the db right now.
+ * 
+ * 2) It's not taking into account any of the search parameters yet.
+ * 
+ * 3) Is this even how we want to handle getting a random recipe?
+ *    It seems like it would be nicer to have the post request w/
+ *      some AJAX and update the recipeDisplay based on the response,
+ *      so we wouldn't have to do a page refresh.
+ */
+
+router.post('/', (req, res, next) => {
+  const randomRecipe = Math.floor(Math.random() * 3) + 1;
+
+  Categories.getAll((err, categories) => {
+    Cuisines.getAll((err, cuisines) => {
+      Diets.getAll((err, diets) => {
+        Recipes.getById(randomRecipe, (err, recipe) => {
+          Recipes.getIngredientsByRecipeId(randomRecipe, (err, ingredients) => {
+            res.render('index', {
+              page: 'Home',
+              menuId: 'home',
+              cuisines: cuisines,
+              categories: categories,
+              diets: diets,
+              recipe: recipe[0],
+              ingredients: ingredients,
+            });
+          });
+        });
+      });
+    });
+  });
+});
+
 
 module.exports = router;
