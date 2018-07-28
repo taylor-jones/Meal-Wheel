2569d6e1a16ed742d3bc4deff7320d39ec9ea5f4
Taylor Jones <taylorjonesdev@gmail.com>
2018-07-28 18:08:57 -0500

    Updated recipe browse functionality to include categories, cuisines, and diets

--- app/controllers/Recipe.js
+++ app/controllers/Recipe.js
@@ -15,9 +15,30 @@ exports.getAll = (callback) => {
     r.created_date
   FROM recipe AS r;`, (err, rows) => {
     if (err) return callback(err, null);
-    callback(null, rows);
+
+    let pending = rows.length;
+
+    for (let i in rows) {
+      const row = rows[i];
+
+      this.getIngredients(row.recipe_id, (err, ingredients) => {
+        this.getCuisines(row.recipe_id, (err, cuisines) => {
+          this.getDietaryRestrictions(row.recipe_id, (err, restrictedDiets) => {
+            row.ingredients = ingredients;
+            row.cuisines = cuisines;
+            row.restrictedDiets = restrictedDiets;
+            // this blocks the funciton from returning until
+            //  after all the recipes have been processed.
+            if (0 === --pending) {
+              callback(null, rows);
+            }
+          });
+        });
+      });
+    }
   });
 };
+
 
 exports.getById = (id, callback) => {
   db.get().query(`
@@ -34,19 +55,36 @@ exports.getById = (id, callback) => {
   FROM recipe AS r
   WHERE r.recipe_id = ?;
   `, id, (err, rows) => {
-    if (err) {
-      callback(err);
-    } else {
-      callback(null, rows);
+    if (err) return callback(err);
+
+    let pending = rows.length;
+
+    for (let i in rows) {
+      const row = rows[i];
+
+      this.getIngredients(row.recipe_id, (err, ingredients) => {
+        this.getCuisines(row.recipe_id, (err, cuisines) => {
+          this.getDietaryRestrictions(row.recipe_id, (err, restrictedDiets) => {
+            row.ingredients = ingredients;
+            row.cuisines = cuisines;
+            row.restrictedDiets = restrictedDiets;
+            // this blocks the funciton from returning until
+            //  after all the recipes information has been gathered
+            if (0 === --pending) {
+              callback(null, rows);
+            }
+          });
+        });
+      });
     }
   });
 };
 
 
-exports.getIngredientsByRecipeId = (id, callback) => {
+exports.getIngredients = (id, callback) => {
   db.get().query(`
-  SELECT
-    ri.ingredient_id,
+    SELECT
+      ri.ingredient_id,
       i.ingredient_name,
       ri.amount,
       u.unit_of_measure_name
@@ -56,6 +94,46 @@ exports.getIngredientsByRecipeId = (id, callback) => {
       INNER JOIN ingredient AS i ON ri.ingredient_id = i.ingredient_id
     WHERE ri.recipe_id = ?
     ORDER BY ri.amount DESC;
+  `, id, (err, rows) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, rows);
+    }
+  });
+};
+
+
+exports.getDietaryRestrictions = (id, callback) => {
+  db.get().query(`
+    SELECT DISTINCT
+      d.dietary_restriction_id,
+      d.dietary_restriction_name
+    FROM dietary_restriction AS d
+    INNER JOIN food_group_dietary_restriction AS fd ON d.dietary_restriction_id = fd.dietary_restriction_id
+    INNER JOIN ingredient AS i ON i.food_group_id = fd.food_group_id
+    WHERE i.ingredient_id IN(
+      SELECT ri.ingredient_id FROM recipe_ingredient AS ri WHERE ri.recipe_id = ?)
+    ORDER BY d.dietary_restriction_name;
+  `, id, (err, rows) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, rows);
+    }
+  });
+};
+
+
+exports.getCuisines = (id, callback) => {
+  db.get().query(`
+    SELECT
+      c.cuisine_id,
+      c.cuisine_name
+    FROM recipe AS r
+      INNER JOIN recipe_cuisine AS rc ON rc.recipe_id = r.recipe_id
+      INNER JOIN cuisine AS c ON rc.cuisine_id = c.cuisine_id
+    WHERE r.recipe_id = ?;
   `, id, (err, rows) => {
     if (err) {
       callback(err);
--- app/public/javascripts/browse.js
+++ app/public/javascripts/browse.js
@@ -3,7 +3,7 @@ $(function() {
    * Cache DOM
    */
 
-  const recipes = Array.from(recipeList); // recipeList is declared in browse.ejs
+  const recipes = recipeList; // recipeList is declared in browse.ejs
   const $recipeItem = $('.recipe-item');
   const $category = $('#recipe-category');
   const $cuisine = $('#recipe-cuisine');
@@ -16,6 +16,24 @@ $(function() {
    */
 
   $category.change(() => {
+    updateRecipeDisplay();
+  });
+
+  $cuisine.change(() => {
+    updateRecipeDisplay();
+  });
+
+  $diet.change(() => {
+    updateRecipeDisplay();
+  });
+
+
+  
+  /**
+   * Functions
+   */
+
+  function getCategoryFilter() {
     const val = sanitize($category.val());
     let matches = undefined;
 
@@ -25,30 +43,67 @@ $(function() {
       });
     }
 
-    updateRecipeVisibility(matches);
-  });
+    return matches || recipes;
+  }
+
+
+  function getCuisineFilter() {
+    const val = sanitize($cuisine.val());
+    let matches = undefined;
+
+    if (val) {
+      matches = recipes.filter((recipe) => {
+        return recipe.cuisines.some((cuisine) => {
+          return cuisine.cuisine_id === val;
+        });
+      });
+    }
+
+    return matches || recipes;
+  }
+
+
+  function getDietFilter() {
+    const val = sanitize($diet.val());
+    let matches = undefined;
+
+    if (val) {
+      matches = recipes.filter((recipe) => {
+        return recipe.restrictedDiets.every((diet) => {
+          return diet.dietary_restriction_id != val;
+        });
+      });
+    }
+
+    return matches || recipes;
+  }
 
 
 
-  /**
-   * Functions
-   */
+  // looks at each of the recipe filters and shows only the
+  //   recipes which meet the criteria of all the filters.
+  function updateRecipeDisplay() {
+    // hide all the recipes by default.
+    $recipeItem.hide();
 
-  /* Displays only the recipes that have an id in an array of recipes,
-    unless [matches] is falsy, then it shows all recipes. */
-  function updateRecipeVisibility(matches) {
-    if (matches) {
-      // hide all the items by default, then show the items in the list.
-      $recipeItem.hide();
+    // then show only the recipes which match all filters.
+    const filters = [
+      getCategoryFilter(),
+      getCuisineFilter(),
+      getDietFilter(),
+    ];
 
-      for (let i = 0; i < matches.length; i++) {
-       $(`#recipe-${matches[i].recipe_id}`).show();
-      }
+    const matches = recipes.filter((r) => {
+      return filters.every((f) => {
+        return f.some((el) => el.recipe_id === r.recipe_id);
+      });
+    });
 
-    } else {
-      $recipeItem.show();
+    for (let i = 0; i < matches.length; i++) {
+      $(`#recipe-${matches[i].recipe_id}`).show();
     }
   }
+
 
 
   // checks if a value is numeric.
