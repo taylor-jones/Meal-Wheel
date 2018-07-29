56040592d531af72a173e7663f1fa0eb5d4f8976
Taylor Jones <taylorjonesdev@gmail.com>
2018-07-29 13:59:23 -0500

    More updates to addRecipe.*

--- app/routes/addRecipe.js
+++ app/routes/addRecipe.js
@@ -4,20 +4,23 @@ const router = express.Router();
 const UnitsOfMeasure = require('../controllers/UnitOfMeasure');
 const Categories = require('../controllers/RecipeCategory');
 const Cuisines = require('../controllers/Cuisine');
-
+const Ingredients = require('../controllers/Ingredient');
 
 /* GET add recipe page. */
 router.get('/', (req, res, next) => {
   Categories.getAll((err, categories) => {
     Cuisines.getAll((err, cuisines) => {
       UnitsOfMeasure.getAll((err, units) => {
-        res.render('addRecipe', {
-          page: 'Submit a new Recipe',
-          menuId: 'add-recipe',
-          cuisines: cuisines,
-          categories: categories,
-          units: units,
-          session: req.session,
+        Ingredients.getAll((err, ingredients) => {
+          res.render('addRecipe', {
+            page: 'Submit a new Recipe',
+            menuId: 'add-recipe',
+            cuisines: cuisines,
+            categories: categories,
+            ingredients: ingredients,
+            units: units,
+            session: req.session,
+          });
         });
       });
     });
