3a9196ef9253d7a0f1bc14124bb4c8ff27c08472
asoback <31739297+asoback@users.noreply.github.com>
2018-07-29 13:21:33 -0500

    added inputs for editing cuisines

---
+++ app/public/javascripts/cuisine.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', pageLoad);
+
+function pageLoad(){
+	//get the count of cuisines
+	const req = new XMLHttpRequest();
+    req.open('POST', 'cuisines', true);
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.addEventListener('load', function() {
+      if (req.status >= 200 && req.status < 400) {
+        const res = JSON.parse(req.responseText);
+        console.log(res);
+        bindButtons(res.cuisineCount);
+      }
+    });
+    var context = null;
+    req.send(context);
+
+}
+
+function bindButtons(count){
+	console.log("Bind buttons test");
+	console.log(count);
+
+
+};
