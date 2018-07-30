6c770602052882308179babc3453f539c30b5c8f
Taylor Jones <taylorjonesdev@gmail.com>
2018-07-29 19:34:41 -0500

    admin cuisine updates

--- app/public/javascripts/admin.js
+++ app/public/javascripts/admin.js
@@ -1,10 +1,67 @@
 $(function() {
   // NOTE: in order for this to work, the delete button must have the matching class.
 
+  /**
+   * EVENT HANDLERS
+   */
+
   // uses functions in admin.js
-  $('.btn-delete').click(function() {
+  $(document).on('click', '.btn-delete', function() {
     deleteRecord(this, window.requestRoute);
   });
+
+  $(document).on('click', '.btn-add', function() {
+    addTableRow();
+  });
+
+  $(document).on('click', '.btn-edit', function() {
+    editRecord(this, window.requestRoute);
+  });
+
+  $(document).on('click', '.btn-save', function() {
+    saveRecord(this, window.requestRoute);
+  });
+
+
+
+  /**
+   * FUNCTIONS
+   */
+
+  /**
+   * adds a new row to the current admin table.
+   */
+  function addTableRow() {
+    const $lastRow = $('tr:last');
+    const $newRow = $lastRow.clone();
+
+    $newRow.find('input').attr('value', '').attr('id', '');
+    $newRow.find('button').attr('id', '');
+    $lastRow.after($newRow);
+
+    editRecord($newRow.find('.btn-edit'), window.requestRoute);
+  }
+
+
+  /**
+   * places the table row in a state of 'edit'
+   */
+  function editRecord(btn, route) {
+    const $btn = $(btn);
+    const $id = $btn.attr('id');
+    const recordId = $id.replace('edit_', '').replace('-', '');
+
+    $btn.siblings('.btn-save').toggleClass('d-none');
+    $btn.toggleClass('d-none');
+    $btn.parents('tr').children('td:nth-of-type(2)').children('input').focus();
+
+    if ($id) {
+      console.log('record exists');
+    } else {
+      console.log('new record');
+    }
+
+  }
 
 
   /**
@@ -22,13 +79,31 @@ $(function() {
   function deleteRecord(btn, route) {
     const $id = $(btn).attr('id');
     const recordId = $id.replace('delete_', '').replace('-', '');
-
     const req = new XMLHttpRequest();
+
     req.open('DELETE', `/${route}/${recordId}`, true);
     req.addEventListener('load', () => {
       if (req.status >= 200 && req.status < 400) {
         removeTableRow($id);
+      } else {
+        console.log(JSON.parse(req.responseText));
+      }
+    });
 
+    req.send();
+  }
+
+
+
+  function saveRecord(btn, route) {
+    const $id = $(btn).attr('id');
+    const recordId = $id.replace('edit_', '').replace('-', '');
+    const req = new XMLHttpRequest();
+
+    req.open('DELETE', `/${route}/${recordId}`, true);
+    req.addEventListener('load', () => {
+      if (req.status >= 200 && req.status < 400) {
+        removeTableRow($id);
       } else {
         console.log(JSON.parse(req.responseText));
       }
