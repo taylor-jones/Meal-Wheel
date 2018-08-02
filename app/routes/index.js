3f9366cb1f13932efdfa0d37277e01ad5cda619f
Taylor Jones <taylorjonesdev@gmail.com>
2018-08-02 16:58:11 -0500

    Updated index route to recognize user's liked/disliked recipes

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,6 +5,7 @@ const Categories = require('../controllers/RecipeCategory');
 const Cuisines = require('../controllers/Cuisine');
 const Diets = require('../controllers/DietaryRestriction');
 const Recipes = require('../controllers/Recipe');
+const Users = require('../controllers/User');
 
 
 /* GET home page. */
@@ -45,57 +46,73 @@ router.post('/', (req, res, next) => {
     context.user = req.session.user.user_id;
   }
 
-  // moved the HTML to the server so we can check for session.
-  Recipes.getByFilter(context, (err, recipe) => {
-    const result = recipe[0];
-    let renderHTML;
 
-    if (!result) {
-      renderHTML = `
-      <div class="row align-items-center justify-content-center text-center">
-        <div class="col-sm-8 col-md-6 col-lg-4">
-          <div class="card mb-5" id="${result.recipe_id}">
-            <div class="card-body">
-              <h5 class="card-title">No Matching Recipe</h5>
-              <p class="card-text">Try chaging some parameters and spin again!</p>
+  Users.getById(context.user, (err, user) => {
+    // moved the HTML to the server so we can check for session.
+    Recipes.getByFilter(context, (err, recipe) => {
+      const result = recipe[0];
+      let renderHTML;
+
+      if (!result) {
+        renderHTML = `
+        <div class="row align-items-center justify-content-center text-center">
+          <div class="col-sm-8 col-md-6 col-lg-4">
+            <div class="card mb-5" id="${result.recipe_id}">
+              <div class="card-body">
+                <h5 class="card-title">No Matching Recipe</h5>
+                <p class="card-text">Try chaging some parameters and spin again!</p>
+              </div>
             </div>
           </div>
-        </div>
-      </div>`;
-    } else {
-      const placeholder = '/images/recipe-placeholder.png';
-      const image = (result.recipe_image_url == null) ? placeholder : result.recipe_image_url;
+        </div>`;
+      } else {
+        const placeholder = '/images/recipe-placeholder.png';
+        const image = (result.recipe_image_url == null) ? placeholder : result.recipe_image_url;
 
-      renderHTML = `
-       <div class="row align-items-center justify-content-center text-center">
-        <div class="col-sm-8 col-md-6 col-lg-4">
-          <div class="card mb-5" id="${result.recipe_id}">
-            <a href="/recipes/${result.recipe_id}">
-              <img class="card-img-top" src="${image}">
-            </a>
-            <div class="card-body">
-              <h5 class="card-title">${result.recipe_name}</h5>
-                <p class="card-text">${result.recipe_description}</p>`;
+        renderHTML = `
+        <div class="row align-items-center justify-content-center text-center">
+          <div class="col-sm-8 col-md-6 col-lg-4">
+            <div class="card mb-5" id="${result.recipe_id}">
+              <a href="/recipes/${result.recipe_id}">
+                <img class="card-img-top" src="${image}">
+              </a>
+              <div class="card-body">
+                <h5 class="card-title">${result.recipe_name}</h5>
+                  <p class="card-text">${result.recipe_description}</p>`;
 
 
-      if (req.session.user) {
+        if (user[0]) {
+          // determine the user's liked & disliked recipes
+          context.likedRecipes = mapSignificantRecipes(user[0].likedRecipes) || context.likedRecipes;
+          context.dislikedRecipes = mapSignificantRecipes(user[0].dislikedRecipes) || context.dislikedRecipes;
+
+          // set the classes of the thumbs-up/down icons to reflect whether liked or disliked.
+          const likedClass = (context.likedRecipes.includes(result.recipe_id) ? 'selected' : '');
+          const dislikedClass = (context.dislikedRecipes.includes(result.recipe_id) ? 'selected' : '');
+
+          renderHTML +=
+            `<span class="card-thumbs text-right">
+              <i class="far fa-thumbs-up recipe-like ${likedClass}" id="recipe-<%= recipes[i].recipe_id %>-like"></i>
+              <i class="far fa-thumbs-down recipe-dislike ${dislikedClass}" id="recipe-<%= recipes[i].recipe_id %>-dislike"></i>
+            </span>`;
+        }
+
         renderHTML +=
-          `<span class="card-thumbs text-right">
-            <i class="far fa-thumbs-up recipe-like" id="recipe-<%= recipes[i].recipe_id %>-like"></i>
-            <i class="far fa-thumbs-down recipe-dislike" id="recipe-<%= recipes[i].recipe_id %>-dislike"></i>
-          </span>`;
+          `</div>
+            </div>
+          </div>
+        </div>`;
       }
 
-      renderHTML +=
-          `</div>
-          </div>
-        </div>
-      </div>`;
-    }
 
-
-    res.send({ recipe: renderHTML });
+      res.send({
+        recipe: renderHTML
+      });
+    });
   });
+
+
+
 });
 
 
@@ -105,6 +122,15 @@ router.get('/logout', (req, res, next) => {
   req.session.destroy();
   res.redirect('/');
 });
+
+
+
+// helper function to map the liked/disliked recipe object to an array.
+function mapSignificantRecipes(jsonArr) {
+  return jsonArr.map((obj) => {
+    return obj.recipe_id;
+  });
+}
 
 
 module.exports = router;
