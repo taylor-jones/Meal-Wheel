2c44d3dd9c9bc91ab0f9008b99d18e263f3ea25e
Taylor Jones <taylorjonesdev@gmail.com>
2018-08-05 21:18:37 -0500

    Updated recipe route to merge browse

--- app/routes/recipe.js
+++ app/routes/recipe.js
@@ -3,17 +3,52 @@ const router = express.Router();
 
 const helpers = require('../helpers');
 
+const Cuisines = require('../controllers/Cuisine');
+const Categories = require('../controllers/RecipeCategory');
+const Diets = require('../controllers/DietaryRestriction');
 const Recipes = require('../controllers/Recipe');
+const Users = require('../controllers/User');
 
 
-/* GET recipe admin page. */
+/* GET browse recipes page. */
 router.get('/', (req, res, next) => {
-  Recipes.getAll((err, recipes) => {
-    res.render('recipe', {
-      page: 'Recipes',
-      menuId: 'recipes',
-      data: recipes,
-      session: req.session,
+  let userId = 0;
+
+  Categories.getAll((err, categories) => {
+    Cuisines.getAll((err, cuisines) => {
+      Diets.getAll((err, diets) => {
+        Recipes.getAll((err, recipes) => {
+
+          // replace the default user (0) with the session
+          //  user, if one exists.
+          if (req.session && req.session.user) {
+            userId = req.session.user.user_id;
+          }
+
+          Users.getById(userId, (err, user) => {
+            const context = {
+              page: 'Browse',
+              menuId: 'browse',
+              cuisines: cuisines,
+              categories: categories,
+              diets: diets,
+              recipes: recipes,
+              session: req.session,
+              likedRecipes: [],
+              dislikedRecipes: [],
+            };
+
+            // if a session user was found, replace the empty liked/disliked
+            //  recipe arrays with the user's own significant recupe ids.
+            if (user[0]) {
+              context.likedRecipes = helpers.mapObjectKey(user[0].likedRecipes, 'recipe_id') || context.likedRecipes;
+              context.dislikedRecipes = helpers.mapObjectKey(user[0].dislikedRecipes, 'recipe_id') || context.dislikedRecipes;
+            }
+
+            res.render('browse', context);
+          });
+        });
+      });
     });
   });
 });
